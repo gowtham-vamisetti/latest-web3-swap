fc6a6121e20d1524cb9a28353af3facaeb3dbe5e
Gowtham Datta <vgowtham1299@gmail.com>
2026-02-17 21:59:42 +0530

    Create app/page.tsx for Web3 swap application

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Web3SwapApp = () => {
+  return (
+    <div>
+      <h1>Web3 Swap Application</h1>
+      <p>This is the main component for the Web3 swap application.</p>
+      {/* Add your swap functionality here */}
+    </div>
+  );
+};
+
+export default Web3SwapApp;
